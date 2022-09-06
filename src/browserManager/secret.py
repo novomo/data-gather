a07f8127dab7a04e74f4674b002fd8599903e24f
root <root@localhost.localdomain>
2022-09-06 07:27:44 +0000

    fix

---
+++ src/browserManager/secret.py
@@ -0,0 +1,51 @@
+TOR_PASS = "RobThe2018!"
+TENNIS_LIVE_BASE_URL = "https://www.flashscore.co.uk/tennis/"
+API_KEY = "1HKWWQC-J6B4EJN-PRTZ9P0-V45YDWP"
+TENNIS_RANKINGS_BASE_URL= "https://www.tennisexplorer.com/ranking"
+API_URL = 'https://api.iquation-corp.com/graphql'
+
+HEADERS = {
+    'Content-Type': 'application/json',
+    'authorization': f"API {API_KEY}"
+}
+
+HOOK_URL = 'https://discordapp.com/api/webhooks/706813737805545503/t6BaL0EufyBzz7-FdWBa887yIBsSM8kuEUhDbqcQA5sSSP9GdgS2vf_JB46V5uH_O0ow'
+
+SPORTS_BASE_URL = 'https://www.sofascore.com'
+ESPORTS_BASE_URL = 'https://egamersworld.com/matches'
+
+BLOGABET_PROFILE = 'https://bet-butler.blogabet.com/'
+
+BETFAIR_HEADERS_GET = {
+    "authority": "scan-inbf.betfair.com",
+    "path": "/www/sports/navigation/facet/v1/search?_ak=nzIFcwyWhrlwYMrh&alt=json",
+    "scheme": "https",
+    "accept": "application/json",
+    "accept-language": "en-US,en;q=0.9",
+    "content-length": "684",
+    "content-type": "application/json",
+    "cookie": "wsid=67ea79b1-56b2-11ec-8719-fa163e363d6a; vid=9dd1e1df-242f-4d7d-8a0f-2cbb2e9bdcc1; storageSSC=lsSSC%3D1; BETEX_ESD=accountservices; bfsd=ts=1638808489610|st=reg; uge=y; xsrftoken=556628308377",
+    "origin": "https://www.betfair.com",
+    "referer": "https://www.betfair.com/",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "sec-gpc": "1"
+}
+
+BETFAIR_HEADERS_MARKET = {
+    "authority": "scan-inbf.betfair.com",
+    "scheme": "https",
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "cookie": "wsid=67ea79b1-56b2-11ec-8719-fa163e363d6a; vid=9dd1e1df-242f-4d7d-8a0f-2cbb2e9bdcc1; storageSSC=lsSSC%3D1; BETEX_ESD=accountservices; bfsd=ts=1638808489610|st=reg; uge=y; xsrftoken=55662a31-8fee-11ec-987b-fa163e7c9ab4; language=en; LPSID-37031206=qbCn2c3OSMasqvNjICRLog; LPVID=QyOTk2MmFmODE3NWRhODc0; loggedIn=true; ccawa=0771058764830837763736725851090340191536; betexPtkSess=betexCurrencySessionCookie%3DGBP%7EbetexLocaleSessionCookie%3Den%7EbetexRegionSessionCookie%3DIRL; ssoid=LkliU6+5jWxyMHHvOUJFy9brz+jGa2yNr3Z/FctnQy8=; exp=ex; betexPtk=betexLocale%3Den~betexRegion%3DIRL~betexCurrency%3DGBP; lka=1652613105633",
+    "origin": "https://www.betfair.com",
+    "referer": "https://www.betfair.com/",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "sec-gpc": "1"
+}
+
+SERVER =  '77.68.13.150'
